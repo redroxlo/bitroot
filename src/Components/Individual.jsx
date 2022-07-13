dcfb6f26dab3baac0e9e7e11b5bbaf0be02d3c3f
redroxlo <redroxlo306@gmail.com>
2022-07-14 03:49:36 +0530

    second commit

--- src/Components/Individual.jsx
+++ src/Components/Individual.jsx
@@ -14,7 +14,7 @@ const Individual = ({ data }) => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
+    width: 300,
     bgcolor: 'background.paper',
     border: '2px solid black',
     boxShadow: 24
